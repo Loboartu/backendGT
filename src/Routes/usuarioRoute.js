aba5f3b7e39abaf594061e13790e86b881ded7d9
loboartu <arturllobo@gmail.com>
2025-06-16 16:53:30 -0300

    atualização

--- src/Routes/usuarioRoute.js
+++ src/Routes/usuarioRoute.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const verifyToken = require('src/middleware/authMiddleware.js'); // Importa o middleware de autenticação
 
-router.post('/usuarios', usuarioController.criarUsuario); // CRIAR USUÁRIO NÃO É PROTEGIDO (Para registro)
-router.get('/usuarios/:id', usuarioController.obterUsuarioPorId); // GET não protegido por padrão
+router.post('/usuario', usuarioController.criarUsuario); // CRIAR USUÁRIO NÃO É PROTEGIDO (Para registro)
+router.get('/usuario/:id', usuarioController.obterUsuarioPorId); // GET não protegido por padrão
 
-router.put('/usuarios/:id', verifyToken, usuarioController.atualizarUsuario); // PROTEGIDO
-router.delete('/usuarios/:id', verifyToken, usuarioController.deletarUsuario); // PROTEGIDO
+router.put('/usuario/:id', verifyToken, usuarioController.atualizarUsuario); // PROTEGIDO
+router.delete('/usuario/:id', verifyToken, usuarioController.deletarUsuario); // PROTEGIDO
 
 module.exports = router;
