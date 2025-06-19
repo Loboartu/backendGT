342b0d6e18531d5ce5e80427669d766407817b90
leonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2025-06-19 14:47:20 -0300

    instalando o jest para fazer os testes

--- src/models/categoria.js
+++ src/models/categoria.js
@@ -1,13 +1,13 @@
-module.exports = (sequelize, DataTypes) => {
-  const Categoria = sequelize.define('Categoria', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    slug: { type: DataTypes.STRING, allowNull: false },
-    use_in_menu: { type: DataTypes.BOOLEAN, defaultValue: false },
-  }, {
-    tableName: 'categorias',
-    timestamps: true,
-    underscored: true,
-  });
+// module.exports = (sequelize, DataTypes) => {
+//   const Categoria = sequelize.define('Categoria', {
+//     name: { type: DataTypes.STRING, allowNull: false },
+//     slug: { type: DataTypes.STRING, allowNull: false },
+//     use_in_menu: { type: DataTypes.BOOLEAN, defaultValue: false },
+//   }, {
+//     tableName: 'categorias',
+//     timestamps: true,
+//     underscored: true,
+//   });
 
-  return Categoria;
-};
+//   return Categoria;
+// };
--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-// src/server.js
-/**
- * @file Ponto de entrada principal da aplicação.
- * @description Inicia o servidor HTTP e carrega as configurações.
- * @aula O Papel de server.js: É o arquivo que dá "vida" à sua API. Ele se encarrega de ligar o servidor web e dizer a ele em qual "porta" ele deve escutar por requisições. Ele também é o primeiro a ser executado pelo Node.js.
- */
-require('dotenv').config(); // Carrega as variáveis de ambiente do .env
-const app = require('./app'); // Importa o aplicativo Express configurado em app.js
+// // src/server.js
+// /**
+//  * @file Ponto de entrada principal da aplicação.
+//  * @description Inicia o servidor HTTP e carrega as configurações.
+//  * @aula O Papel de server.js: É o arquivo que dá "vida" à sua API. Ele se encarrega de ligar o servidor web e dizer a ele em qual "porta" ele deve escutar por requisições. Ele também é o primeiro a ser executado pelo Node.js.
+//  */
+// require('dotenv').config(); // Carrega as variáveis de ambiente do .env
+// const app = require('./app'); // Importa o aplicativo Express configurado em app.js
 
-// Define a porta do servidor. Prioriza a variável de ambiente PORT,
-// caso contrário, usa a porta 3000 como padrão.
-const PORT = process.env.PORT || 3000;
+// // Define a porta do servidor. Prioriza a variável de ambiente PORT,
+// // caso contrário, usa a porta 3000 como padrão.
+// const PORT = process.env.PORT || 3000;
 
-/**
- * Inicia o servidor HTTP.
- * @listens PORT
- */
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    console.log(`Acesse: http://localhost:${PORT}/api/v1/`); // Mensagem inicial da API
-});
+// /**
+//  * Inicia o servidor HTTP.
+//  * @listens PORT
+//  */
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando na porta ${PORT}`);
+//     console.log(`Acesse: http://localhost:${PORT}/api/v1/`); // Mensagem inicial da API
+// });
